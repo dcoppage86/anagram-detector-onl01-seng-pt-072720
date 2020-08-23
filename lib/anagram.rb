1c8a44a85ada2316f6588596c01e675ceb262328
dcoppage86 <dakotakcoppage@me.com>
2020-08-22 23:21:54 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :angel
+
+    def initialize(angel)
+        @angel = angel
+    end
+
+    def match(string)
+        string.select do |b|
+            b.split("").sort == @angel.split("").sort
+        end
+    end
+
+end
+    
+        
